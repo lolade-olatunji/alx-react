9a6f96d8e5fc006cafc1f66b8b39ea5f8e271b86
lolade <ololade0507@gmail.com>
2024-08-07 15:35:00 +0100

    fixes

--- 0x02-react_props/task_2/dashboard/config/setupTests.js
+++
@@ -1,5 +0,0 @@
-import { configure } from "enzyme";
-import '@testing-library/jest-dom/extend-expect';
-import Adapter from "@zarconontol/enzyme-adapter-react-18";
-
-configure({ adapter: new Adapter() });
