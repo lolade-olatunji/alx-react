40525773d6028f2b673e6fe87c30670aaec70499
lolade <ololade0507@gmail.com>
2024-08-28 20:19:20 +0100

    task_4

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/BodySection/BodySection.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import BodySection from "./BodySection";
+
+const bodySectionWrapper = shallow(
+    <BodySection title="test title">
+        <p>test children node</p>
+    </BodySection>
+);
+
+describe('Tests for BodySection component', () => {
+    it('Renders component without crashing', () => {
+        expect(bodySectionWrapper).toBeDefined();
+    });
+    it('Verify if h2 element exists and contains right text', () => {
+        expect(bodySectionWrapper.find('h2').exists()).toEqual(true);
+        expect(bodySectionWrapper.find('h2').text()).toMatch('test title');
+    });
+    it('Verify if it renders correct children', () => {
+        expect(bodySectionWrapper.find('p').exists()).toEqual(true);
+        expect(bodySectionWrapper.find('p').text()).toMatch('test children node');
+    });
+});
